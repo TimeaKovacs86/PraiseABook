dee71a5a7658b41f88e908faff6600ca3e51b056
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-04-20 16:58:36 +0100

    MongoDB connection with secure authentication,  Created requirements.txt and Procfile

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
 
+import pymongo as pymongo
 from flask import Flask, render_template
 
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+CLUSTER = os.getenv("CLUSTER")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+
+client = pymongo.MongoClient(
+    "mongodb+srv://" + USERNAME + ":" + PASSWORD + "@" + CLUSTER + ".mongodb.net/test?retryWrites=true&w=majority")
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+
+    x = collection.find_one()
 
     return render_template('base.html')
 
