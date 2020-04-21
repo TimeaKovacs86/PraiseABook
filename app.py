8ed8b1decd115010da286a3e7b909e5e92e41d2d
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-04-21 16:55:51 +0100

    Made a html page for the recommendation format and connected it with mongoDB, so we can create new objects in the database

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import pymongo as pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
@@ -11,18 +11,45 @@ COLLECTION_NAME = os.getenv("COLLECTION_NAME")
 
 client = pymongo.MongoClient(
     "mongodb+srv://" + USERNAME + ":" + PASSWORD + "@" + CLUSTER + ".mongodb.net/test?retryWrites=true&w=majority")
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
+    book_array = []
 
-    x = collection.find_one()
+    for books in collection.find():
+        book_array.append(books)
 
-    return render_template('base.html')
+    return render_template('base.html', books=book_array)
+
+
+@app.route('/recommendation')
+def recommendation():
+    return render_template('recommend.html')
+
+
+@app.route('/create', methods=["POST"])
+def create():
+    """create a new object in the database"""
+    author = request.form["author"]
+    book_title = request.form["book_title"]
+    genre = request.form["genre"]
+    recommendation = request.form["recommendation"]
+    book_cover_pic = request.form["book_cover_pic"]
+
+    mydict = {
+        "author": author,
+        "title": book_title,
+        "genre": genre,
+        "cover_picture_link": book_cover_pic,
+        "recommendation": recommendation}
+
+    collection.insert_one(mydict)
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
