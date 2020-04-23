f4edad7a1be34c186828ea8e68b21e9d3d7e8907
TimeaKovacs86 <kovacs.timea.86@gmail.com>
2020-04-23 17:17:49 +0100

    Delete function

--- app.py
+++ app.py
@@ -93,5 +93,13 @@ def update(id):
     return render_template("base.html", books=collect_data())
 
 
+@app.route('/delete/<item_id>')
+def delete(item_id):
+    collection.delete_one(
+        {"_id": ObjectId(item_id)},
+    )
+    return render_template("base.html", books=collect_data())
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
